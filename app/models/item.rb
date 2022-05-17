d188bc4e3ff8e796df519041b3496a054ca9494f
Wade Naughton <wadenaughton@gmail.com>
2022-05-17 18:07:24 -0400

    functionality complete

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
 
-  has_many :itemwarehouses
+  has_many :itemwarehouses, dependent: :delete_all
+  has_many :warehouses, through: :itemwarehouses
 
   validates_presence_of :name
-  validates_presence_of :price 
+  validates_presence_of :price
 end
